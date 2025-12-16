4fa960cd372c239f9d109336f550141d2b0fff40
Caio <caiogentilcorado@gmail.com>
2025-12-16 14:16:43 -0500

    Add try and except to polynomial

--- graphingcalc.py
+++ graphingcalc.py
@@ -27,25 +27,28 @@ def poly_graph(a,b,c,d,e):
 menu = promptinput("What kind of graph?",["Trig", "Polynomial"])
 coefficient = []
 if menu == "Polynomial":
-    exponent = int(input("Enter graph degree(max of 4): "))
-    for i in range(exponent+1):
-        coefficient.append(int(input(f"Enter coefficient of the the term with degree {i}: ")))
-    match exponent:
-        case 0:
-            poly_graph(0,0,0,0,coefficient[0])
-        case 1:
-            poly_graph(0,0,0,coefficient[1],coefficient[0])
-        case 2:
-            poly_graph(0,0,coefficient[2],coefficient[1],coefficient[0])
-        case 3:
-            poly_graph(0,coefficient[3],coefficient[2],coefficient[1],coefficient[0])
-        case 4:
-            poly_graph(coefficient[4],coefficient[3],coefficient[2],coefficient[1],coefficient[0])
+    try:
+        exponent = int(input("Enter graph degree(max of 4): "))
+        for i in range(exponent+1):
+            coefficient.append(int(input(f"Enter coefficient of the the term with degree {i}: ")))
+        match exponent:
+            case 0:
+                poly_graph(0,0,0,0,coefficient[0])
+            case 1:
+                poly_graph(0,0,0,coefficient[1],coefficient[0])
+            case 2:
+                poly_graph(0,0,coefficient[2],coefficient[1],coefficient[0])
+            case 3:
+                poly_graph(0,coefficient[3],coefficient[2],coefficient[1],coefficient[0])
+            case 4:
+                poly_graph(coefficient[4],coefficient[3],coefficient[2],coefficient[1],coefficient[0])
+    except:
+        print("Enter valid numbers!")
 else:
     trig_type = promptinput("Type of trig?",["Cos", "Sin", "Tan"])
     match trig_type:
         case "Cos":
-            a = int(input("Enter a value"))
+            a = int(input("Enter a value: "))
             with Graph('my graph') as G:
                 f, x = G.f, G.x
                 f[x] = a*sympy.cos()
