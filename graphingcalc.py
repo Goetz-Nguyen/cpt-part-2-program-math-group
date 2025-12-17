7898512bf70e5b246fe5d14cbdabb59ac2288b37
Caio <caiogentilcorado@gmail.com>
2025-12-17 13:50:09 -0500

    Update and fix graphic calculator

--- graphingcalc.py
+++ graphingcalc.py
@@ -20,35 +20,59 @@ def promptinput(message,choices):
     return prompt(user_input)[0] # Return chosen answer
 
 
-def poly_graph(a,b,c,d,e):
-    with Graph('my graph') as G:
-        f, x = G.f, G.x
-        f[x] = a*x**4 + b*x**3 + c*x**2 + d*x + e
-menu = promptinput("What kind of graph?",["Trig", "Polynomial"])
-coefficient = []
-if menu == "Polynomial":
-    try:
-        exponent = int(input("Enter graph degree(max of 4): "))
-        for i in range(exponent+1):
-            coefficient.append(int(input(f"Enter coefficient of the the term with degree {i}: ")))
-        match exponent:
-            case 0:
-                poly_graph(0,0,0,0,coefficient[0])
-            case 1:
-                poly_graph(0,0,0,coefficient[1],coefficient[0])
-            case 2:
-                poly_graph(0,0,coefficient[2],coefficient[1],coefficient[0])
-            case 3:
-                poly_graph(0,coefficient[3],coefficient[2],coefficient[1],coefficient[0])
-            case 4:
-                poly_graph(coefficient[4],coefficient[3],coefficient[2],coefficient[1],coefficient[0])
-    except:
-        print("Enter valid numbers!")
-else:
-    trig_type = promptinput("Type of trig?",["Cos", "Sin", "Tan"])
-    match trig_type:
-        case "Cos":
+menu = promptinput("What kind of graph?",["Trig", "Polynomial", "Exit"])
+error = False
+while menu != "Exit":
+    if menu == "Polynomial":
+        try:
+            coefficient = []
+            equation = ""
+            exponent = int(input("Enter graph degree: "))
+            for i in range(exponent+1):
+                coefficient.append(int(input(f"Enter coefficient of the the term with degree {i}: ")))
+            for i in range(len(coefficient), 0, -1):
+                if i != 0:
+                    equation += f"{coefficient[i-1]}*x^{i-1} + "
+            equation = equation[:len(equation)-3]
+            error = False
+        except:
+            error = True
+            print("Enter valid numbers!")
+        if not error:
+            try:
+                with Graph('my graph') as G:
+                        f, x = G.f, G.x
+                        f[x] = equation
+            except:
+                print("Couldn't graph!")
+            menu = promptinput("What kind of graph?",["Trig", "Polynomial"])
+    else:
+        trig_type = promptinput("Type of trig?",["Cos", "Sin", "Tan"])
+        try:
             a = int(input("Enter a value: "))
-            with Graph('my graph') as G:
-                f, x = G.f, G.x
-                f[x] = a*sympy.cos()
+            k = int(input("Enter k value: "))
+            d = int(input("Enter d value: "))
+            c = int(input("Enter c value: "))
+            error = False
+        except:
+            error = True
+            print("Enter valid inputs!")
+        if not error:
+            try:
+                match trig_type:
+                    case "Cos":
+                        with Graph('my graph') as G:
+                            f, x = G.f, G.x
+                            f[x] = a*sympy.cos(k*(x-d))+c
+                    case "Sin":
+                        with Graph('my graph') as G:
+                            f, x = G.f, G.x
+                            f[x] = a*sympy.sin(k*(x-d))+c
+                    case "Tan":
+                        with Graph('my graph') as G:
+                            f, x = G.f, G.x
+                            f[x] = a*sympy.tan(k*(x-d))+c
+            except:
+                print("Couldn't graph!")
+            menu = promptinput("What kind of graph?",["Trig", "Polynomial"])
+print("Bye!")
