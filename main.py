935d122c8aaeecf563aeaec2b950181e92617f21
Caio <caiogentilcorado@gmail.com>
2026-01-07 13:48:55 -0500

    Add scientific calculator

--- main.py
+++ main.py
@@ -1405,12 +1405,152 @@ def shaper():
             return
         matplotlib.pyplot.show()
 
+def scientific_calc():
+    history = []
+
+
+    class Calculation_history:
+        def __init__(self, history: list[str]):
+            self._history = history
+    
+        def get_history(self) -> None:
+            spot_history = 1
+
+            for equation in self._history:
+                print(f"{spot_history}. {equation}")
+                spot_history += 1
+
+        def reset_history(self) -> None:
+            with open("Calculator history.txt", "w") as file:
+                pass
+
+    def menu() -> None:
+
+        mode = "DEG" #rad/deg
+
+        while True:
+            option = promptinput(
+                "-----------------------------------------------------",
+                ["{ 1. Calculate }", "{ 2. Change mode: RAD/DEG}", "{ 3. How to use the calculator }", "{ 4. Get calculator history }",
+                "{ 5. Clear calculator history file }", "{ 6. Exit }"])
+            match option:
+                case "{ 1. Calculate }":
+                    os.system('cls' if os.name == 'nt' else 'clear')
+                    calculate(history, mode)
+
+                case "{ 2. Change mode: RAD/DEG}":
+                    if mode == "DEG":
+                        mode = "RAD"
+                    else:
+                        mode = "DEG"
+                    print(f"Mode: |{mode}|")
+
+                case "{ 3. How to use the calculator }":
+                    information()
+            
+                case "{ 4. Get calculator history }":
+                    os.system('cls' if os.name == 'nt' else 'clear')
+                    show_history = Calculation_history(history)
+                    show_history.get_history()
+
+                case "{ 5. Clear calculator history file }":
+                    os.system('cls' if os.name == 'nt' else 'clear')
+                    output_history = Calculation_history(history)
+                    output_history.reset_history()
+
+                case "{ 6. Exit }":
+                    return
+
+    def information():
+        # (use: '+' '-' '/' '*' '**' 'sqrt(x)')
+        # how to use in rad and deg and log(x, base)
+        pass
+
+
+    def test_calculate() -> bool:
+        return True #test
+    
+    def calculate(history: list[str], mode: str) -> None:
+        while True:
+            option = promptinput(
+                "Would you like to input a calculation/calculations",
+                ["{ 1. Calculate }", "{ 2. Go back to menu }"])
+        
+            os.system('cls' if os.name == 'nt' else 'clear')
+            match option:
+                case "{ 1. Calculate }":
+                    while True:
+                        original_eq = str(input("Equation: "))
+                        equation = original_eq.split()
+                        
+                        #if not true retry
+                        if test_calculate() == True:
+                            #use alt to evaluate expression
+                            if mode == "DEG":
+
+                                for i in range(len(equation)):
+                                    start = 0
+                                    end = 0
+                                    final = ""
+
+                                    for j in range(len(equation[i])):
+                                        if equation[i][j] == "(":
+                                            start = j + 1
+
+                                        elif equation[i][j] == ")":
+                                            end = j
+                                        
+                                        num = equation[i][start:end]
+
+                                    if "sin" in  equation[i]:
+                                        equation[i] = f"{sympy.sin((int(num) * math.pi)/180)}"
+                                        
+                                    elif "cos" in  equation[i]:
+                                        equation[i] = f"{sympy.cos((int(num) * math.pi)/180)}"
+
+                                    elif "tan" in  equation[i]:
+                                        equation[i] = f"{sympy.tan((int(num) * math.pi)/180)}"
+                                        
+                                for new in equation:
+                                    final += new
+                                result = sympify(final)
+
+                                final_result= f"{round(result, 2):.2f}"
+                                
+                                result_text = f"| {original_eq} = {final_result} |"
+                                print(result_text)
+                                history.append(result_text)
+
+                                with open("Calculator history.txt", "a+") as file:
+                                    file.writelines(result_text + "\n")
+                                            
+                            else:
+                                result = sympify(equation)
+                                result_text = f"| {equation} = {result} |"
+                                print(result_text)
+
+                                history.append(result_text)
+
+                                with open("Calculator history.txt", "a+") as file:
+                                    file.writelines(result_text + "\n")
+
+                            next_option = promptinput(
+                            "Would you like to input a calculation again?",
+                            choices= ["{ 1. Yes }", "{ 2. Go back }"])
+                            os.system('cls' if os.name == 'nt' else 'clear')
+                            if next_option == "{ 2. Go back }":
+                                break
+                case "{ 2. Go back to menu }":  
+                    break
+                        
+    menu()
+
 
 def main():
     tests = {}
     menu_options = ["Create new test", "Load test from file", "Run a test",
                      "Study Guides", "Math Definitions", "Graph Calculator", "Mental Math",
-                     "Simple Calculator", "Geometry Calculator",  
+                     "Simple Calculator", "Scientific Calculator", "Geometry Calculator",  
                      "Draw Shapes", "Exit"]
     choice = promptinput("What do you want?", menu_options)
     while choice != "Exit":
@@ -1458,6 +1598,10 @@ def main():
                 simple_calculator()
                 choice = promptinput("What do you want?", menu_options)
             
+            case "Scientific Calculator":
+                scientific_calc()
+                choice = promptinput("What do you want?", menu_options)
+
             case "Geometry Calculator":
                 geometry_calc()
                 choice = promptinput("What do you want?", menu_options)
