dcd488f3a323fbe7cbf09f52b9460c90e770255e
Gabriel Paras <gabbyuparas@gmail.com>
2026-01-08 13:59:42 -0500

    Added dosctrings and comments to geometry calc

--- main.py
+++ main.py
@@ -1251,124 +1251,279 @@ def simple_calculator():
 
 def geometry_calc():
     class Rectangle:
+        """
+        A class that handles on solving the perimeter and area of a rectangle
+
+        Attributes:
+            length (int): Length of the rectangle 
+            width (int): Width of the rectangle
+
+        Invariants:
+            - Cannot have an empty input 
+            - Cannot have a negative input
+        """       
         def __init__(self, length: int, width: int) -> None:
+            """
+            Initializes the value of length and width of the rectangle
+            """
             self._length = length
             self._width = width
         def rectangle_perimeter(self):
+            """
+            Method for solving the perimeter of a rectangle
+            """
             r_perimeter = 2 * (self._length + self._width)
-            print(r_perimeter)
+            print(f"The perimeter of the rectangle is {r_perimeter} units.")
         def rectangle_area(self):
+            """
+            Method for solving the area of a rectangle
+            """
             r_area = self._length * self._width
-            print(r_area)
+            print(f"The area pf the rectangle is {r_area} units.")
 
 
     class Square:
+        """
+        A class that handles on solving the perimeter and area of a square
+
+        Attributes:
+            s (int): Side of the square
+
+        Invariants:
+            - Cannot have an empty input 
+            - Cannot have a negative input
+        """    
         def __init__(self, s: int):
+            """
+            Initializes the value of the side of a square
+            """
             self._s = s
         def square_perimeter(self):
+            """
+            Method for solving the perimeter of a square
+            """
             sq_perimeter = 4 * self._s
-            print(sq_perimeter)
+            print(f"The perimeter of the square is {sq_perimeter} units.")
         def square_area(self):
+            """
+            Method for solving the area of a square
+            """
             sq_area = self._s**2
-            print(sq_area)
-
-
+            print(f"The area of the square is {sq_area} units.")
 
     class Triangle:
-        def __init__(self, a: int, b: int, c: int, base:int , height: int,):
+        """
+        A class that handles on solving the perimeter and area of a triangle
+
+        Attributes:
+            a (int): Side 'a' value
+            b (int): Side 'b' value
+            c (int): Side 'c' value
+            base (int): Base of the triangle
+            height (int): Height of the triangle
+
+        Invariants:
+            - Cannot have an empty input 
+            - Cannot have a negative input
+        """ 
+        def __init__(self, a: int, b: int, c: int, base:int , height: int):
+            """
+            Initializes the value of the side of a square
+            """
             self._a = a
             self._b = b
             self._c = c
             self._base = base
             self._height = height
         def triangle_perimeter(self):
+            """
+            Method for solving the perimeter of a triangle
+            """
             t_perimeter = self._a + self._b + self._c
-            print(t_perimeter)
+            print(f"The perimeter of the triangle is {t_perimeter} units")
         def triangle_area(self):
+            """
+            Method for solving the area of a triangle
+            """
             t_area = (self._base * self._height) / 2
-            print(f"{t_area:.2f}")
+            print(f"The area of the triangle is {t_area:.2f} units")
 
     class Circle:
+        """
+        A class that handles on solving the diameter, radius, and circumference of a circle
+
+        Attributes:
+            length (int): Length of the rectangle 
+            width (int): Width of the rectangle
+
+        Invariants:
+            - Cannot have an empty input 
+            - Cannot have a negative input
+        """ 
         def __init__(self, diameter: int, radius: int, circumference: int) -> None:
+            """
+            Initializes the value of values of the diameter, radius, and circumference of a circle
+            """
             self._diameter = diameter
             self._radius = radius
             self._circumference = circumference
             self.pi = math.pi
         def diameter(self):
+            """
+            Method for solving the diamter of a circle
+            """
             cir_diameter = 2 * self._radius
-            print(f"{cir_diameter:.2f}")
+            print(f"The diameter of the circle is {cir_diameter:.2f} units.")
         def circumference(self):
+            """
+            Method for solving the circumference of a circle
+            """
             cir_circumference = 2 * self.pi * self._radius
-            print(f"{cir_circumference:.2f}")
+            print(f"The circumference of the circle is {cir_circumference:.2f} units.")
+        def radius(self):
+            """
+            Method for solving the radius of a circle
+            """
+            cir_radius = self._circumference / (2 * self.pi)
+            print(f"The circumference of the circle is {cir_radius:.2f} units.")
         def circle_area(self):
+            """
+            Method for solving the area of a circle
+            """
             cir_area = self.pi * self._radius**2
-            print(f"{cir_area:.2f}")
+            print(f"The area of the circle is {cir_area:.2f} units.")
+
 
     # 3D Shapes
     class Cube(Square):
+        """
+        A class that handles on solving the volume and surface area of the cube
+
+        Attributes:
+            s (int): Side of the cube
+
+        Invariants:
+            - Cannot have an empty input 
+            - Cannot have a negative input
+        """ 
         def __init__(self, s: int):
+            """
+            Initializes the value of the side of a cube
+            """
             super().__init__(s)
         def cube_volume(self):
+            """
+            Method for solving the volume of a cube
+            """
             c_volume = self._s**3
-            print(c_volume)
+            print(f"The volume of the cube is {c_volume} units.")
         def cube_surface_area(self):
+            """
+            Method for solving the surface area of the cube 
+            """
             c_surface_area = 6*self._s**2
-            print(c_surface_area)
+            print(f"The surface area of the cube is {c_surface_area} units.")
+
     class Rectangular_Prism(Rectangle):
+        """
+        A class that handles on solving the volume and surface area of the rectangular prism
+
+        Attributes:
+            length (int): Length of the rectangular prism
+            width (int): Width of the rectangular prism
+            height (int): Height of the rectangular prism
+
+        Invariants:
+            - Cannot have an empty input 
+            - Cannot have a negative input
+        """ 
         def __init__(self, length: int, width: int, height: int):
+            """
+            Initializes the value of the length, width, and height of the rectangular prism
+            """
             super().__init__(length, width)
             self._height = height
         def rect_prism_volume(self):
+            """
+            Method of solving the volume of a rectangular prism
+            """
             r_prism_volume = self._length * self._width * self._height
-            print(r_prism_volume)
+            print(f"The volume of the rectangular prism is {r_prism_volume} units.")
         def rect_prism_surface_area(self):
+            """
+            Method of solving the surface area of a rectangular prism
+            """
             r_prism_surface_area = 2*(self._length * self._width) + 2*(self._length * self._height) + 2*(self._width * self._height)
-            print(r_prism_surface_area)
+            print(f"The surface area of the rectangle is {r_prism_surface_area} units. ")
 
-
-    print("------------------------------------------------------------")
-    # USE CHAT for the formula cannot do by me and sebastian
-    # class Pyramid(Triangle):
-    #     def __init__(self, base: int, height: int, length: int, width: int):
-    #         self._length = length
-    #         self._width = width
-    #         self._length = length
-    #         self._width = width
-    #     def pyramid_volume(self):
-    #         p_volume = (self._length * self._base * self._height) / 3
-    #         print(p_volume)
-    #     def pyramid_surface_area(self):
-    #         p_surface_area = ((self._length * self._width) + ((1/2) * self._width)) * math.sqrt(((4*self._height)**2) + self._length**2) + ((1/2*self._length) * math.sqrt((4*self._height)**2 + self._width**2))
-    #         print(p_surface_area)
     class Circular_cylinder:
+        """
+        A class that handles on solving the volume and surface area of a circular cylinder
+
+        Attributes:
+            radius (int): Radius of the circular cylinder
+            height (int): Height of the circular cylinder
+
+        Invariants:
+            - Cannot have an empty input 
+            - Cannot have a negative input
+        """ 
         def __init__(self, radius: int, height:int):
+            """
+            Initializes the value of the radius and height of a cylinder
+            """
             self._radius = radius
             self._height = height
         def cylinder_volume(self):
+            """
+            Method for solving the volume of a cylinder 
+            """
             cy_volume = math.pi * self._radius**2 * self._height
-            print(f"{cy_volume:.2f}")
+            print(f"The volume of the cylinder is {cy_volume:.2f} units.")
         def cylinder_surface_area(self):
+            """
+            Method for solving the surface area of a cylinder 
+            """
             cy_surface_area = (2 *  math.pi * self._radius * self._height) + (2 * math.pi * self._radius**2)
-            print(f"{cy_surface_area:.2f}")
+            print(f"The surface area of the cylinder is {cy_surface_area:.2f} units.")
 
     class Cone(Circular_cylinder):
+        """
+        A class that handles on solving the volume and surface area of a cone
+
+        Attributes:
+            radius (int): Radius of the circular cylinder
+            height (int): Height of the circular cylinder
+
+        Invariants:
+            - Cannot have an empty input 
+            - Cannot have a negative input
+        """ 
         def __init__(self, radius: int, height: int):
+            """
+            Initializes the value of the radius and height of a cone
+            """
             super().__init__(radius, height)
         def cone_volume(self):
+            """
+            Method for solving the volume of a cone
+            """
             cne_volume = math.pi * self._radius**2 * (self._height / 3)
-            print(f"{cne_volume:.2f}")
+            print(f"The volume of the cone is {cne_volume:.2f} units")
         def cone_surface_area(self):
+            """
+            Method for solving the surface area of a cone 
+            """
             cne_surface_area = math.pi * self._radius * math.sqrt(self._height**2 + self._radius**2)
-            print(f"{cne_surface_area:.2f}")
+            print(f"The surface area of the cone is {cne_surface_area:.2f} units.")
 
 
-    def menu():
-        shapeslist = ["Square", "Rectangle", "Triangle", "Circle", "Cube", "Rectangular Prism",
-                    "Circular Cylinder", "Cone", "Exit"]
-        menus = promptinput("Pick shape:", shapeslist)
-        os.system('cls' if os.name == 'nt' else 'clear')
-        while menus != "Exit":
-            match menus:
+    def main():
+        while True:
+            shapeslist = ["Square", "Rectangle", "Triangle", "Circle", "Cube", "Rectangular Prism",
+                        "Circular Cylinder", "Cone", "Exit"]
+            menu = promptinput("Pick shape:", shapeslist)
+            match menu:
                 case "Square":
                     try: 
                         sq1 = Square(int(input("Enter side length: ")))
@@ -1376,8 +1531,6 @@ def geometry_calc():
                         print("Enter valid numbers")
                     sq1.square_perimeter()
                     sq1.square_area()
-                    menus = promptinput("Pick shape:", shapeslist)
-                    os.system('cls' if os.name == 'nt' else 'clear')
                 case "Rectangle":
                     try:
                         rect1 = Rectangle(int(input("Enter length: ")), int(input("Enter width: ")))
@@ -1385,8 +1538,6 @@ def geometry_calc():
                         print("Enter valid numbers")
                     rect1.rectangle_area()
                     rect1.rectangle_perimeter()
-                    menus = promptinput("Pick shape:", shapeslist)
-                    os.system('cls' if os.name == 'nt' else 'clear')
                 case "Triangle":
                     try:
                         tri1 = Triangle(int(input("Enter side a: ")), int(input("Enter side b: ")), int(input("Enter side c: ")), int(input("Enter base: ")), int(input("Enter height: ")))
@@ -1394,8 +1545,6 @@ def geometry_calc():
                         print("Enter valid numbers")
                     tri1.triangle_perimeter()
                     tri1.triangle_area()
-                    menus = promptinput("Pick shape:", shapeslist)
-                    os.system('cls' if os.name == 'nt' else 'clear')
                 case "Circle":
                     try:
                         cir1 = Circle(int(input("Enter diameter: ")), int(input("Enter radius: ")), int(input("Enter circumference: ")))
@@ -1403,9 +1552,8 @@ def geometry_calc():
                         print("Enter valid numbers")
                     cir1.diameter()
                     cir1.circumference()
+                    cir1.radius()
                     cir1.circle_area()
-                    menus = promptinput("Pick shape:", shapeslist)
-                    os.system('cls' if os.name == 'nt' else 'clear')
                 case "Cube":
                     try: 
                         cub = Cube(int(input("Enter side value: ")))
@@ -1413,8 +1561,6 @@ def geometry_calc():
                         print("Invalid Value")
                     cub.cube_volume()
                     cub.cube_surface_area()
-                    menus = promptinput("Pick shape:", shapeslist)
-                    os.system('cls' if os.name == 'nt' else 'clear')
                 case "Rectangular Prism":
                     try: 
                         rectangular_p = Rectangular_Prism(int(input("Enter length: ")), int(input("Enter width: ")), int(input("Enter height: ")))
@@ -1422,8 +1568,6 @@ def geometry_calc():
                         print("Invalid value")
                     rectangular_p.rect_prism_volume()
                     rectangular_p.rect_prism_surface_area()
-                    menus = promptinput("Pick shape:", shapeslist)
-                    os.system('cls' if os.name == 'nt' else 'clear')
                 case "Circular Cylinder":
                     try:
                         cylinder = Circular_cylinder(int(input("Enter radius: ")), int(input("Enter height:  ")))
@@ -1431,8 +1575,6 @@ def geometry_calc():
                         print("Enter valid number")
                     cylinder.cylinder_volume()
                     cylinder.cylinder_surface_area()
-                    menus = promptinput("Pick shape:", shapeslist)
-                    os.system('cls' if os.name == 'nt' else 'clear')
                 case "Cone":
                     try:
                         cne = Cone(int(input("Enter radius: ")), int(input("Enter height: ")))
@@ -1440,8 +1582,6 @@ def geometry_calc():
                         print("Enter valid values")
                     cne.cone_volume()
                     cne.cone_surface_area()
-                    menus = promptinput("Pick shape:", shapeslist)
-                    os.system('cls' if os.name == 'nt' else 'clear')
                 case "Exit":
                     return
 
