2183a549387755370defefa9d6b0bec8945a973a
Caio <caiogentilcorado@gmail.com>
2026-01-08 13:11:10 -0500

    Add docstring to shape drawer

--- shaper.py
+++ shaper.py
@@ -20,27 +20,38 @@ def promptinput(message,choices):
     return prompt(user_input)[0] # Return chosen answer
 
 
-fig, ax = matplotlib.pyplot.subplots()
-shaper = promptinput("What shape?", ["Circle", "Rectangle/Square"])
-if shaper == "Circle":
-    radius = float(input("Enter radius: "))
-    ax.add_patch(Circle((radius*4/2, radius*4/2), radius=radius, edgecolor="lightblue", facecolor="lightblue"))
-    ax.set_xlim(0,radius*4)
-    ax.set_ylim(0,radius*4)
-    ax.set_aspect('equal')
-    matplotlib.pyplot.grid(True)
-    matplotlib.pyplot.title("Circle")
-    ax.set_yticks(numpy.arange(0, radius*4+1, radius//2))
-    ax.set_xticks(numpy.arange(0, radius*4+1, radius//2))
-else:
-    width = float(input("Enter width: "))
-    height = float(input("Enter height: "))
-    ax.add_patch(Rectangle((width, height), width=width, height=height, edgecolor="lightblue", facecolor="lightblue"))
-    ax.set_xlim(0, width*3)
-    ax.set_ylim(0, height*3)
-    ax.set_aspect('equal')
-    matplotlib.pyplot.grid(True)
-    matplotlib.pyplot.title("Rectangle/Square")
-    ax.set_yticks(numpy.arange(0, height*3+1, height//2))
-    ax.set_xticks(numpy.arange(0, width*3+1, width//2))
-matplotlib.pyplot.show()
+def shaper():
+    """
+    Draw the desired shape in the cartestian plane
+    """
+    #TUTORIALS VV
+    #https://www.geeksforgeeks.org/python/how-to-draw-shapes-in-matplotlib-with-python/
+    #https://matplotlib.org/stable/gallery/mplot3d/index.html
+    while True:
+        fig, ax = matplotlib.pyplot.subplots() # Creates the plane
+        shaper = promptinput("What shape?", ["Circle", "Rectangle/Square", "Exit"])
+        if shaper == "Circle":
+            radius = float(input("Enter radius: "))
+            ax.add_patch(Circle((radius*4/2, radius*4/2), radius=radius, edgecolor="lightblue", facecolor="lightblue"))
+            ax.set_aspect('equal')
+            matplotlib.pyplot.grid(True)
+            matplotlib.pyplot.title("Circle")
+            ax.set_yticks(numpy.arange(0, radius*4+1, radius//2))
+            ax.set_xticks(numpy.arange(0, radius*4+1, radius//2))
+        elif shaper == "Rectangle/Square":
+            width = float(input("Enter width: "))
+            height = float(input("Enter height: "))
+            ax.add_patch(Rectangle((width, height), width=width, height=height, edgecolor="lightblue", facecolor="lightblue"))
+            ax.set_xlim(0, width*3)
+            ax.set_ylim(0, height*3)
+            ax.set_aspect('equal')
+            matplotlib.pyplot.grid(True)
+            matplotlib.pyplot.title("Rectangle/Square")
+            ax.set_yticks(numpy.arange(0, height*3+1, height//2))
+            ax.set_xticks(numpy.arange(0, width*3+1, width//2))
+        else: 
+            return
+        matplotlib.pyplot.show()
+        shaper = promptinput("What shape?", ["Circle", "Rectangle/Square", "Exit"])
+
+shaper()
